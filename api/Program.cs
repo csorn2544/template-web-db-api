229ac04cc52f8194964044a2c229b759371bed04
chanisornananda <chanisorn@ananda.co.th>
2024-02-07 12:57:07 +0700

    removeNecessaryLib

--- api/Program.cs
+++ api/Program.cs
@@ -1,8 +1,3 @@
-using Autofac;
-using Autofac.Extensions.DependencyInjection;
-using Microsoft.EntityFrameworkCore;
-using Serilog;
-using System.Text.Encodings.Web;
 using api.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
