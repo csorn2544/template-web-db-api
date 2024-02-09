f80e9ebc92193f1d1f34c4bb05ab908881d13195
chanisornananda <151501711+chanisornananda@users.noreply.github.com>
2024-02-09 15:14:57 +0700

    AddingPdpaPrivacy

---
+++ infra/Repositories/PdpaPrivacyRepository.cs
@@ -0,0 +1,11 @@
+﻿using domain.Entities.PDPA;
+using domain.IRepositories.PDPA;
+using infra.Persistence;
+using infra.Repositories.Base;
+
+namespace infra.Repositories
+{
+    internal class PdpaPrivacyRepository(_dbContext context): BaseRepository<PdpaPrivacy,_dbContext>(context), IPdpaPrivacyRepository
+    {
+    }
+}
