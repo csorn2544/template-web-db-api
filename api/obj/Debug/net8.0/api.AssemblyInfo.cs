a1a96e0b8c4f5446643ed8408f762aa94c8c3880
chanisorn <aue_chanisor@hotmail.com>
2024-03-08 15:33:50 +0700

    addgitIgnore

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e33b2dbdca972e78835fe3bae49f78a1b2b1c71b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c2b5521c1f3517df7aee7673c40a2ff34a12a6c")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
